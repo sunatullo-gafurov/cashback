0110966ba1a0ec5b6dc2972f5ead1de4d0d17439
Sunatullo Gafurov <me@gmail.com>
2020-01-07 11:08:38 +0500

    Added incresed and special purchases

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
-const regularPurchases = 20000;
+const regularPurchases = 2000;
 const regularPurchasesPercent = 0.01;
-let cashback = regularPurchases * regularPurchasesPercent;
+
+const increasedPurchases = 10000;
+const increasedPurchasesPercent = 0.05;
+
+const specialPurchase = 100;
+const specialPurchasePercent = 0.03;
+
+let cashback = regularPurchases * regularPurchasesPercent + increasedPurchases * increasedPurchasesPercent + specialPurchase * specialPurchasePercent;
 const cashbackLimit = 3000;
 if (cashback > cashbackLimit) {
     cashback = 2000;
